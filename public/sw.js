8b025ac11b6d4e9457076dcdb3c790a0c7fd6220
Vinicius Santos <viniciussouza742@gmail.com>
2026-07-29 00:23:39 -0300

    chore: bump service worker v3 para forcar atualizacao dos apps instalados

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'monvy-v2';
+const CACHE = 'monvy-v3';
 const SHELL = ['/', '/index.html', '/favicon.svg', '/icon.svg', '/manifest.webmanifest'];
 self.addEventListener('install', (e) => { e.waitUntil(caches.open(CACHE).then((c) => c.addAll(SHELL)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', (e) => { e.waitUntil(caches.keys().then((ks) => Promise.all(ks.filter((k) => k !== CACHE).map((k) => caches.delete(k)))).then(() => self.clients.claim())); });
